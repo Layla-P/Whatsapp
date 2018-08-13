2c1799fd12836e3c2353b959022a27dfea546334
layla-p <layla.porter@gmail.com>
2018-08-13 12:02:06 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace WhatsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwilioClient.Init(
+                Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID"),
+                Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN")
+            );
+
+            var message = MessageResource.Create(
+                from: new PhoneNumber("whatsapp:TWILIO_WHATSAPP_NUMBER"),
+                to: new PhoneNumber("whatsapp:YOUR_MOBILE_NUMBER"),
+                body: "Ahoy from Twilio!"
+            );
+
+            Console.WriteLine("Message SID: " + message.Sid);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# WhatsApp Demo
+A demo of implementing 
+The Twilio API for WhatsApp in a dotnet core console app
+
+
